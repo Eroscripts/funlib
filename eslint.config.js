4c6874105a847b474bf266e69ddd917568aeefab
Dimava <dimava2@ya.ru>
2025-04-17 22:48:23 +0300

    workspace: init

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+
+})
+  .overrideRules({
+    'no-console': 'off',
+    'antfu/no-top-level-await': 'off',
+    'antfu/if-newline': 'off',
+    'style/max-statements-per-line': 'off',
+    'one-var': 'off',
+    'node/prefer-global/process': ['warn', 'always'],
+  })
