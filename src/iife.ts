e53291eebc5bd61c2d344433cfbfbacbcefe2aa3
Dimava <dimava2@ya.ru>
2025-04-29 21:08:34 +0300

    [[stash]]: player

---
+++ src/iife.ts
@@ -0,0 +1,13 @@
+import * as Fun from '.'
+import { drawFunscriptGraph, drawFunscriptsCanvas, makeGradient } from './canvas'
+import { TCodePlayer } from './player'
+
+Object.assign(window, {
+  Fun: {
+    ...Fun,
+    TCodePlayer,
+    drawFunscriptGraph,
+    makeGradient,
+    drawFunscriptsCanvas,
+  },
+})
---
+++ src/player.ts
@@ -0,0 +1,64 @@
+import type { Funscript } from '.'
+
+export class TCodePlayer {
+  video: HTMLVideoElement | undefined
+  funscript: Funscript | undefined
+
+  constructor(video: HTMLVideoElement | undefined, funscript: Funscript | undefined) {
+    this.video = video
+    this.funscript = funscript
+  }
+
+  port?: SerialPort
+  writer?: WritableStreamDefaultWriter<string>
+
+  async requestPort() {
+    this.port = await navigator.serial.requestPort()
+    await this.port.open({ baudRate: 115200 })
+    const encoder = new TextEncoderStream()
+    encoder.readable.pipeTo(this.port.writable!)
+    this.writer = encoder.writable.getWriter()
+  }
+
+  write(output: string) {
+    output = output.replaceAll('_', '')
+    if (!output.trim()) return
+    console.log('TCodePlayer: Writing', JSON.stringify(output))
+    this.writer?.write(output)
+  }
+
+  async run() {
+    while (true) {
+      await new Promise(requestAnimationFrame)
+      const tcode = this.tCodeForState()
+      if (tcode) {
+        this.write(tcode)
+      }
+    }
+  }
+
+  tcodeOptions = { format: true, precision: 2 }
+  prevState = { paused: true, currentTime: 0, seeking: false }
+  tCodeForState(): string {
+    if (!this.video || !this.funscript) return ''
+
+    const { paused, currentTime, seeking } = this.video
+    let at = currentTime * 1000
+    let tcode = ''
+    if (seeking) { // Jumped
+      tcode = this.funscript.getTCodeAt(at).toString(this.tcodeOptions)
+      if (!paused) {
+        tcode = tcode.slice(0, -1) + ' '
+        tcode += this.funscript.getTCodeFrom(at).toString(this.tcodeOptions)
+      }
+    } else if (!this.prevState.paused && paused) { // Paused
+      tcode = this.funscript.getTCodeAt(at).toString(this.tcodeOptions)
+    } else if (this.prevState.paused && !paused) { // Unpaused
+      tcode = this.funscript.getTCodeFrom(at).toString(this.tcodeOptions)
+    } else if (!paused) { // Playing
+      tcode = this.funscript.getTCodeFrom(at, this.prevState.currentTime * 1000).toString(this.tcodeOptions)
+    }
+    this.prevState = { paused, currentTime, seeking }
+    return tcode
+  }
+}
